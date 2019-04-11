8910beb670735940c6ab2c27a7b4356c374bf7e8
arulk8 <arulmurugan194@gmail.com>
2019-04-11 08:04:09 +0530

    started routing

---
+++ src/components/order/CheckoutSummary/CheckoutSummary.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const CheckoutSummary = props => {
+  return (
+    <div>
+      <h1> we hope it taates well!</h1>
+      <div style={{ width: '300px', height: '300px' }} />
+    </div>
+  );
+};
+
+export default CheckoutSummary;
